f093071fbcbe3fa22bbf7e3ee0504945eb235b0e
Essence Alexander <upperlefty@zoho.eu>
2018-05-29 15:00:57 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+let values = [1, 2, 3];
+for (let num of values) {
+    console.log(num);
+ }
+ // Code outputs
+ 1
+ 2
+ 3
+ 
+ let o  = {
+ 
+      createIterator(items) {
+           for (let i = 0;  < items.length; i++)  {
+               yield items[i];
+           }
+        }
+    };
+    let iterator  = o.createIterator([1, 2, 3]);
+    
+    
+    // Creating Iterables
+    
+      let collection  = {
+          items:  [],
+          *[Symbol.iterator]() {
+              for (let item of this.items)  {
+                    yield item;
+                   
