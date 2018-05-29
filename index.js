22cad8eb4b9845e6b433b7dcf722440ae80d95b2
Essence Alexander <upperlefty@zoho.eu>
2018-05-29 15:26:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,3 +36,20 @@ for (let num of values) {
     for (let x of collection) {
         console.log(x);
     }
+
+// Code outputs 
+
+1
+2
+3
+
+let colors  =  [ "red", "green", "blue" ];
+let tracking = new Set([1234, 5678, 9012]);
+let data  =  new Map();
+
+data.set("title", "The Catcher & the Rye");
+data.set("format", "ebook");
+
+for (let entry of colors.entries()) {
+    console.log(entry);
+}
